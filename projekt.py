73fbfa5029702788a55d3b369c764a46865f5e5a
Tanuló 301-17 <301-17@KKSZKI.LOCAL>
2021-11-16 17:44:52 +0100

    lul

--- projekt.py
+++ projekt.py
@@ -1,2 +1,6 @@
-print("yoyoyo")
-print("oyoyoy SECUND BRAH")
+szam1=int(input("Kérem az első számot: "))
+szam2=int(input("Kérem a második számot: "))
+
+osszeg=szam1+szam2
+
+print(str(szam1)+" + "+str(szam2)+" = "+str(osszeg))
