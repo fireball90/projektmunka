4fc282ad52e05a49b51346531d0bea6b6764f593
Tanuló 301-17 <301-17@KKSZKI.LOCAL>
2021-11-16 18:49:20 +0100

    kész verzió

--- feljelszo.py
+++ feljelszo.py
@@ -1,19 +1,36 @@
-name=str(input("Kérem a felhasználó nevét: "))
-password=str(input("Kérem a jelszavát: "))
+def belepes():
+    name=str(input("Kérem a felhasználó nevét: "))
+    password=str(input("Kérem a jelszavát: "))
 
-nameList=["fireball","norbert","kyywa"]
-passwordList=["asd123","LekiBOI","allinkah69"]
+    nameList=["fireball","norbert","kyywa"]
+    passwordList=["asd123","LekiBOI","allinkah69"]
 
-check=0
+    check=0
 
-for i in range(len(nameList)):
-    if (nameList[i] == name) and (passwordList[i] == password):
-        check=1
+    for i in range(len(nameList)):
+        if (nameList[i] == name) and (passwordList[i] == password):
+            check=1
+        else:
+            pass
+
+
+    if check == 1:
+        print("Sikeres bejelentkezés!")
     else:
-        pass
+        print("Hibás felhasználónév / jelszó!")
+        ujra()
+    
+
+def ujra():
+    again=input("Akar újra próbálkozni? (i/n)  ")
+
+    if again == 'i' or again == 'I':
+        belepes()
+    elif again == 'n' or again == 'N':
+        print("Viszlát!")
+        print("______________________________________")
+    else:
+        ujra()
 
 
-if check == 1:
-    print("Sikeres bejelentkezés!")
-else:
-    print("Hibás felhasználónév / jelszó!")
+belepes()
